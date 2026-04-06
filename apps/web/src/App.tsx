6f029c0c15bb3848726969e3b6c5f6dfb905d433
Aliaksandr Kavalenka <40535982+aneekee@users.noreply.github.com>
2026-04-06 20:17:51 +0200

    3-chore-split-build-by-pages (#10)
    * split by pages
    
    * don't cache

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,28 +1,44 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 
 import { Toaster } from '@/shared/components/ui/sonner';
 
-import { HomePage } from './features/home/pages/Home.page';
-import { LoginPage } from './features/auth/pages/Login.page';
-import { NotFoundPage } from './features/auth/pages/NotFound.page';
 import { WithAuth } from './features/auth/components/WithAuth';
+
+const HomePage = lazy(() =>
+  import('./features/home/pages/Home.page').then((m) => ({
+    default: m.HomePage,
+  })),
+);
+const LoginPage = lazy(() =>
+  import('./features/auth/pages/Login.page').then((m) => ({
+    default: m.LoginPage,
+  })),
+);
+const NotFoundPage = lazy(() =>
+  import('./features/auth/pages/NotFound.page').then((m) => ({
+    default: m.NotFoundPage,
+  })),
+);
 
 export const App = () => {
   return (
     <>
       <Toaster />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <WithAuth>
-              <HomePage />
-            </WithAuth>
-          }
-        />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <WithAuth>
+                <HomePage />
+              </WithAuth>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
