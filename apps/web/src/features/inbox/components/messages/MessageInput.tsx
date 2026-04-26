c452959753c62ad5fa89f6bf2001a711bc241984
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2026-04-26 12:30:37 +0200

    add multiline

--- apps/web/src/features/inbox/components/messages/MessageInput.tsx
+++ apps/web/src/features/inbox/components/messages/MessageInput.tsx
@@ -33,7 +33,7 @@ export const MessageInput = ({ chatId }: Props) => {
   };
 
   return (
-    <div className="flex gap-2 items-end">
+    <div className="flex gap-2 items-center">
       <div className="grow">
         <Textarea
           value={message}
@@ -49,6 +49,7 @@ export const MessageInput = ({ chatId }: Props) => {
             }
           }}
           maxLength={255}
+          data-enable-grammarly="false"
         />
       </div>
       <div>
--- apps/web/src/features/inbox/components/messages/TextMessage.tsx
+++ apps/web/src/features/inbox/components/messages/TextMessage.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const TextMessage = ({ textContent, date }: Props) => {
   return (
     <div className="relative py-1 px-2 w-fit text-sm rounded-lg bg-gray-200">
-      <p>
+      <p className="whitespace-pre-wrap">
         {textContent}
         <span className="inline-block w-14" aria-hidden="true" />
       </p>
