8cd08bbc6ab33b0279d646d69d9113bd33d9851c
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2026-04-29 21:28:16 +0200

    add performance interceptor

---
+++ apps/api/src/shared/interceptors/performance.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class PerformanceInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(PerformanceInterceptor.name);
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    const req = context
+      .switchToHttp()
+      .getRequest<{ method: string; url: string }>();
+
+    const { method, url } = req;
+    const start = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const duration = Date.now() - start;
+        this.logger.log(`${method} ${url} - ${duration.toString()}ms`);
+      }),
+    );
+  }
+}
