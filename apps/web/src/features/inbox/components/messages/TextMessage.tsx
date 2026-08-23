d11cd3ab0e88078bf6309b3da7ff3baeb0db27d2
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2026-08-23 20:49:06 +0200

    fix message text wrap

--- apps/web/src/features/inbox/components/messages/TextMessage.tsx
+++ apps/web/src/features/inbox/components/messages/TextMessage.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const TextMessage = ({ textContent, date }: Props) => {
   return (
     <div className="relative w-full py-1 px-2 text-sm rounded-lg bg-gray-200">
-      <p className="whitespace-pre-wrap">
+      <p className="whitespace-pre-wrap wrap-anywhere">
         <TextWithLinks text={textContent} />
         <span className="inline-block w-14" aria-hidden="true" />
       </p>
